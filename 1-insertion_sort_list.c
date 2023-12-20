f3b4140743f3ef6a6cdf126eadb3be3b483e6575
Taofeek Olusola <olusolataofeek@gmail.com>
2023-12-20 08:34:19 +0100

    sorting_algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,53 +1,53 @@
 #include "sort.h"
 /**
- * my_swap - swap 2 element in an list
- * @head: head of list
- * @a: node
- * @b: node
- */
+* my_swap - swap 2 element in an list
+* @head: head of list
+* @a: node
+* @b: node
+*/
 void my_swap(listint_t *x, listint_t *y, listint_t **head)
 {
-    listint_t *aux1 = NULL, *aux2 = NULL;
-    if (x == NULL || y == NULL)
-        return;
-    aux1 = x->prev;
-    aux2 = y->next;
-    /* if nodes are adjacent*/
-    if (aux1)
-        aux1->next = y;
-    if (aux2)
-        aux2->prev = x;
-    x->next = aux2;
-    x->prev = y;
-    y->next = x;
-    y->prev = aux1;
-    if (aux1 == NULL)
-        *head = y;
+listint_t *aux1 = NULL, *aux2 = NULL;
+if (x == NULL || y == NULL)
+return;
+aux1 = x->prev;
+aux2 = y->next;
+/* if nodes are adjacent*/
+if (aux1)
+aux1->next = y;
+if (aux2)
+aux2->prev = x;
+x->next = aux2;
+x->prev = y;
+y->next = x;
+y->prev = aux1;
+if (aux1 == NULL)
+*head = y;
 }
 /**
- * insertion_sort_list  - insertion_sort_list
- * @list: doubly liked list
- *
- */
+* insertion_sort_list  - insertion_sort_list
+* @list: doubly liked list
+*
+*/
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *head, *prev;
-    int val;
-    if (list == NULL || (*list)->next == NULL || (*list) == NULL)
-    {
-        return;
-    }
-    head = *list;
-    while (head)
-    {
-        prev = head->prev;
-                val = head->n;
-        while (prev && prev->n > val)
-        {
-            my_swap(prev, head, list);
-            print_list(*list);
-            prev = head->prev;
-        }
-        head = head->next;
-    }
+listint_t *head, *prev;
+int val;
+if (list == NULL || (*list)->next == NULL || (*list) == NULL)
+{
+return;
+}
+head = *list;
+while (head)
+{
+prev = head->prev;
+val = head->n;
+while (prev && prev->n > val)
+{
+my_swap(prev, head, list);
+print_list(*list);
+prev = head->prev;
+}
+head = head->next;
+}
 }
--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,34 +1,34 @@
 #include "sort.h"
 /**
- * shell_sort -Sort an array using shell_sort algorithm
- * @array: array
- * @size: size of the array
- * Return: NULL
- **/
+* shell_sort -Sort an array using shell_sort algorithm
+* @array: array
+* @size: size of the array
+* Return: NULL
+**/
 void shell_sort(int *array, size_t size)
 {
-    unsigned int gap = 1, i, j;
-    int tmp;
-    if (array == NULL)
-        return;
-    if (size < 2)
-        return;
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            tmp = array[i];
-            j = i;
-            while (j >= gap && array[j - gap] > tmp)
-            {
-                array[j] = array[j - gap];
-                j -= gap;
-            }
-            array[j] = tmp;
-        }
-        print_array(array, size);
-        gap /= 3;
-    }
+unsigned int gap = 1, i, j;
+int tmp;
+if (array == NULL)
+return;
+if (size < 2)
+return;
+while (gap < size / 3)
+gap = gap * 3 + 1;
+while (gap > 0)
+{
+for (i = gap; i < size; i++)
+{
+tmp = array[i];
+j = i;
+while (j >= gap && array[j - gap] > tmp)
+{
+array[j] = array[j - gap];
+j -= gap;
+}
+array[j] = tmp;
+}
+print_array(array, size);
+gap /= 3;
+}
 }
